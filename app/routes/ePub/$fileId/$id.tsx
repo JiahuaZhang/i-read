dc24e8fcf9ae34bfc19517b8889b928af89339a8
Jiahua Zhang <hughzhanghz@gmail.com>
2022-07-14 10:40:25 +0800

    fix import

--- app/routes/ePub/$fileId/$id.tsx
+++ app/routes/ePub/$fileId/$id.tsx
@@ -1,14 +1,14 @@
-import { type LoaderFunction, type LinksFunction } from "@remix-run/node";
-import { useRecoilValue } from "recoil";
+import { type LinksFunction, type LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import arialFont from "public/font/Arial/font.css";
+import fangZhengFont from "public/font/FangZheng/font.css";
+import georgiaFont from "public/font/Georgia/font.css";
+import helveticaFont from "public/font/Helvetica/font.css";
+import tahomaFont from "public/font/Tahoma/font.css";
+import { useRecoilValue } from "recoil";
 import { PageNavigationBar } from "~/components/ePub/PageNavigationBar";
 import { getCurrentEpubChapter } from "~/utils/google.drive.server";
 import { bookConfigState } from "~/utils/state/book.config";
-import arialFont from "/public/font/Arial/font.css";
-import fangZhengFont from "/public/font/FangZheng/font.css";
-import georgiaFont from "/public/font/Georgia/font.css";
-import helveticaFont from "/public/font/Helvetica/font.css";
-import tahomaFont from "/public/font/Tahoma/font.css";
 
 export const links: LinksFunction = () => {
   return [
