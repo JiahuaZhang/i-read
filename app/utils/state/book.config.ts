0f57c150a2c30b191440ce61de4c690d0c52f923
Jiahua Zhang <hughzhanghz@gmail.com>
2022-07-19 10:47:32 +0800

    add recoil-sync

--- app/utils/state/book.config.ts
+++ app/utils/state/book.config.ts
@@ -1,17 +1,30 @@
+import { number, object, string } from '@recoiljs/refine';
 import { atom } from 'recoil';
+import { syncEffect } from 'recoil-sync';
 
 type BookConfig = {
   fontSize: number;
   chinseFontFamily: string;
   englishFontFamily: string;
   fontFamily: string;
-  // fieldId: string;
+  fieldId: string;
   // currentPage: string;
 };
 
-const defaultBookConfig: BookConfig = { fontSize: 28, chinseFontFamily: '', englishFontFamily: '', fontFamily: '' };
+const defaultBookConfig: BookConfig = { fontSize: 28, chinseFontFamily: '', englishFontFamily: '', fontFamily: '', fieldId: '' };
 
 export const bookConfigState = atom({
   key: 'bookConfigState',
-  default: defaultBookConfig
+  default: defaultBookConfig,
+  effects: [
+    syncEffect({
+      refine: object({
+        fontSize: number(),
+        chinseFontFamily: string(),
+        englishFontFamily: string(),
+        fontFamily: string(),
+        fieldId: string()
+      })
+    })
+  ]
 });
