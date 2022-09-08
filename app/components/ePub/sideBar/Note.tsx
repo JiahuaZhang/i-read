7fe03209ce418517a9dd398c6b77a2e596e3db58
Jiahua Zhang <hughzhanghz@gmail.com>
2022-09-07 19:29:36 -0500

    enable sort notes

--- app/components/ePub/sideBar/Note.tsx
+++ app/components/ePub/sideBar/Note.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { ImageHighlight } from '~/utils/selection/ImageNote';
 import { Highlight } from '~/utils/selection/Note';
@@ -25,13 +26,41 @@ const renderHighlight = (highlight: Highlight, index: number, _: Highlight[]) =>
   return <li key={index}>Invalid Highlight</li>;
 };
 
+enum Order {
+  ascend = '▲',
+  descend = '▼'
+}
+
 export const Note = () => {
   // todo
-  // 1, show all of the highlights
-  // 2, sort by created or natural order
   // 3, can toggle select, all, none, some
   // 4, one click copy
   const [highlights, setHighlights] = useRecoilState(highlightState);
+  const [order, setOrder] = useState(Order.ascend);
 
-  return <ul>{highlights.map(renderHighlight)}</ul>;
+  console.log(highlights);
+
+  return <div>
+    <div>
+      <button
+        className='rounded px-2 py-1 bg-blue-400 text-white'
+        onClick={() => setOrder(
+          prev => prev === Order.ascend ? Order.descend : Order.ascend
+        )}>
+        Sort {order}
+      </button>
+    </div>
+    <ul>{[...highlights].sort(
+      (a, b) => {
+        switch (order) {
+          case Order.ascend:
+            return a.compareTo(b);
+          case Order.descend:
+            return b.compareTo(a);
+          default:
+            return 0;
+        }
+      }
+    ).map(renderHighlight)}</ul>;
+  </div>;
 };
