50eff4fd1ebe8f9793490a53f88810f86aaeb3a4
Jiahua Zhang <hughzhanghz@gmail.com>
2024-08-19 04:00:52 -0400

    add debug log

--- app/util/database.ts
+++ app/util/database.ts
@@ -3,11 +3,14 @@ import _ from 'lodash';
 import { BookConfig, defaultBookConfig } from './state/book.config';
 
 const BOOK_CONFIG_TABLE = 'epub-book-config';
-const supabase = createClient('https://zepxgzybtlkrokmlavpy.supabase.co', import.meta.env.VITE_SUPABASE_KEY);
+console.log('logging environment variables for fly.io');
+console.log(import.meta.env);
+console.log(process.env);
+const supabase = createClient('https://zepxgzybtlkrokmlavpy.supabase.co', import.meta.env.VITE_SUPABASE_KEY ?? process.env.VITE_SUPABASE_KEY);
 
 export const getBookConfig = async () => {
   const { data } = await supabase.from(BOOK_CONFIG_TABLE).select().eq('id', 1);
-  if (!data) return defaultBookConfig;
+  if (!data?.length) return defaultBookConfig;
   return data[0].config as BookConfig;
 };
 
@@ -15,5 +18,3 @@ export const updateBookConfig = async (config: BookConfig) => {
   if (_.isEqual(config, defaultBookConfig)) return;
   await supabase.from(BOOK_CONFIG_TABLE).upsert({ config, id: 1 });
 };
-
-export const getNotes = async () => {};
